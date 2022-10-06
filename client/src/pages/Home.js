6df51e4cde7a85af8e3eb1472e2e52d1bc299c8a
it-jcg <joyce.garnett@magna.com>
2022-10-06 15:06:01 -0400

    fixed up styling in homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,12 @@ const Home = () => {
     return (
         <main className="center-horizontal">
             <div className="container">
-                <p className="center-text">To view the event bracket, enter the Tournament ID provided by your organizer.</p>
+                <h2>Welcome to Tournament Bracket!</h2>
                 <div className="line" />
             </div>
             <div className="container">
+                <p className="center-text">To view the event bracket, enter the Tournament ID provided by your organizer.</p>
                 <form>
-                    <label htmlFor="tournamentId">Tournament ID</label>
                     <input type="text" id="tournamentId" name="tournamentId" placeholder="Tournament ID"></input>
                     <div className="center-horizontal">
                         <input type="submit" className="button" value="Enter" id="findTournament"></input>
