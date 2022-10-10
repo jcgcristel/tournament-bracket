fe8a2d87ba7e03f7fde6b404b887cf19d24fef53
Jaryd Knight <jaryd.knight@hotmail.com>
2022-10-10 04:56:08 -0400

    add comments

--- server/utils/generateTournament.js
+++ server/utils/generateTournament.js
@@ -1,6 +1,9 @@
+// Match class import
 import Match from './generateMatch.js';
+// uuid imported to create unique IDs for tournament
 import {v4 as uuidv4 } from 'uuid';
 
+// Tournament class which will create instances of torunament as well as generate tournaments
 class Tournament {
     constructor(tournamentName, username, tournamentSize, teams) {
         this.tournamentName = tournamentName,
@@ -9,6 +12,7 @@ class Tournament {
         this.tournamentSize = tournamentSize,
         this.teams = teams,
         this.matches = []
+        // Final round used to tell how many rounds a tournament should have so that generateMatches knows when to stop recursivelly calling itself
         this.finalRound = (function() {
             switch (tournamentSize) {
                 case 4:
@@ -23,10 +27,10 @@ class Tournament {
         })()
     }
 
+    // Class method to generate tournaments. Starts by generating championship match then works backwards to generate two preceding matches. Continues recursively until first round reached
     generateMatches(round=this.finalRound, nextMatch='') {
         if (round > 0) {
             let match = new Match(this.tournamentId, round, nextMatch)
-            console.log(match)
             this.matches.push(match);
 
             round -= 1;
@@ -40,10 +44,37 @@ class Tournament {
     }
 }
 
-let tournament = new Tournament('test', 'user1', 4, ['team1', 'team2'])
+// Added "type": "module" to package.json to test funcion quickly
 
-tournament.generateMatches();
+// EXAMPLE USE OF TOURNAMENT CLASS:
+// let tournament = new Tournament('test', 'user1', 4, ['team1', 'team2'])
 
-console.log(tournament.finalRound)
+// tournament.generateMatches();
+
+// Output:
+
+// Match {
+//     tournamentId: 'c28ec84a-14eb-439d-88c3-8ea862a2e276',
+//     round: 2,
+//     matchId: '0091ac2b-6007-47ce-8ecc-b330ac9c4b88',
+//     teams: [],
+//     nextMatch: ''
+//   }
+//   Match {
+//     tournamentId: 'c28ec84a-14eb-439d-88c3-8ea862a2e276',
+//     round: 1,
+//     matchId: '646d42f1-a65d-4fc2-9139-30286737dc51',
+//     teams: [],
+//     nextMatch: '0091ac2b-6007-47ce-8ecc-b330ac9c4b88'
+//   }
+//   Match {
+//     tournamentId: 'c28ec84a-14eb-439d-88c3-8ea862a2e276',
+//     round: 1,
+//     matchId: '66473202-fb25-4338-882a-7003f8d0d019',
+//     teams: [],
+//     nextMatch: '0091ac2b-6007-47ce-8ecc-b330ac9c4b88'
+//   }
+
+// console.log(tournament.finalRound)
 
 export default Tournament;
