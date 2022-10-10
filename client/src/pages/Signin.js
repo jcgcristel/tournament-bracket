3cc9dc1a6daf74dec7b060bd9f76b588fd654dde
rheneadianne <rheneadianne@gmail.com>
2022-10-10 17:52:10 -0400

    added sign in

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,17 +1,41 @@
-const Signin = () => {
+import { LOGIN_USER } from "../utils/mutations";
+
+const Signin = (props) => {
+    const [inputState, setInputState] = useState({username: '', password: ''})
+    const [login, { error }] = useMutation(LOGIN_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setInputState({ ...inputState, [name]: value, }); 
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+          const { data } = await addUser({
+            variables: { ...inputState },
+          });
+    
+          Auth.login(data.addUser.token);
+        } catch (e) {
+          console.error(e);
+        }
+      };
+
     return (
         <main className="center-horizontal">
             <div className="container">
                 <h2>Sign In</h2>
                 <div className="line" />
-                <form>
+                <form onSubmit={handleFormSubmit}>
                     <div className="input-group">
                         <label htmlFor="username">USERNAME</label>
-                        <input type="text" id="username" name="username" placeholder="Username"></input>
+                        <input type="text" id="username" name="username" placeholder="Username" onChange={handleChange}></input>
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">PASSWORD</label>
-                        <input type="password" id="password" name="password" placeholder="Password"></input>
+                        <input type="password" id="password" name="password" placeholder="Password" onChange={handleChange}></input>
                     </div>
                     <div className="center-horizontal">
                         <input type="submit" className="button" id="signin" value="Sign In"></input>
