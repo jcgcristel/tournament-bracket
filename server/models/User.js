9ff57a0fe29b7c14824d9bb0c2d5106680bc32fc
rheneadianne <rheneadianne@gmail.com>
2022-10-10 18:01:19 -0400

    added bcrypt, signin tested (it works)

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
   {
@@ -27,6 +28,22 @@ const userSchema = new Schema(
     },
   }
 );
- const User = model('User', userSchema);
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
 
 module.exports = User;
