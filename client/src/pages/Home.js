814c968c5007897aad3f88a7dd11a55aa6f63107
rheneadianne <rheneadianne@gmail.com>
2022-10-12 18:03:02 -0400

    hosted tournment link works

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,23 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
+    const [tournament_ID, setName] = useState('')
+    const handleChange = (event) => {
+        if (event.target.value) {
+            setName(event.target.value);
+        }
+    };
+
+    const handleFormSubmit = async (event, thing) => {
+        event.preventDefault();
+        try {
+            setName('');
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
     return (
         <main className="center-horizontal">
             <div className="container">
@@ -7,10 +26,10 @@ const Home = () => {
             </div>
             <div className="container">
                 <p className="center-text">To view the event bracket, enter the Tournament ID provided by your organizer.</p>
-                <form>
-                    <input type="text" id="tournamentId" name="tournamentId" placeholder="Tournament ID"></input>
+                <form onSubmit={handleFormSubmit(tournament_ID)}>
+                    <input type="text" id="tournamentId" name="tournamentId" placeholder="Tournament ID" onChange={handleChange} value={tournament_ID}></input>
                     <div className="center-horizontal">
-                        <input type="submit" className="button" value="Enter" id="findTournament"></input>
+                        <Link to={`/tournament/${tournament_ID}`}><input type="submit" className="button" value="Enter" id="findTournament"></input></Link>
                     </div>
                 </form>
             </div>
