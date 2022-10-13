1b9e727d465701bfa64dfc26f4e4fc5799648099
it-jcg <joyce.garnett@magna.com>
2022-10-12 21:10:49 -0400

    changed db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bracket-buster', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
